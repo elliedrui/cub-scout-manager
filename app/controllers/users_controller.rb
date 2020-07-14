dd080f6469124fa786420a1e5779f146e6c4e270
jaesonwatts <illender.the.great@gmail.com>
2020-07-14 17:13:39 -0600

    working on defining user/sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  #loading signup form
+  def new 
+  end
+
+  #signup
+  def create
+  end
+
+  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  
+  root 'sessions#welcome'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+
   resources :users
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
