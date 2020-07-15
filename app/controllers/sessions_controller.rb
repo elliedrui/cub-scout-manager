d671d7e055c90e624ff762b4705a390ac07297d4
jaesonwatts <illender.the.great@gmail.com>
2020-07-15 02:04:06 -0600

    omniauth partially added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
 
   #login action
   def create
-    @user = User.find_by(username: params[:user][:username])
+    @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      render :login
+      redirect_to 'login'
     end
   end
 
