6a8c33b529247636234d059dcf70fa27fc3db349
jaesonwatts <illender.the.great@gmail.com>
2020-07-14 18:54:16 -0600

    sign up locally working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,20 @@ class UsersController < ApplicationController
 
   #signup
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_is] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
   
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :email, presence: true, uniqueness: true  
+  
 end
