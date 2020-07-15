2cfc7166407d4baae31322b744d65aaf18e4533a
jaesonwatts <illender.the.great@gmail.com>
2020-07-14 18:25:43 -0600

    sessions route working, gem errors fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'sessions#welcome'
+  root 'sessions#home'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
