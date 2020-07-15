41d58769453d04df55a429b39185c794e5258d3c
jaesonwatts <illender.the.great@gmail.com>
2020-07-14 19:21:30 -0600

    correct render pointer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      render :new
+      render :login
     end
   end
 
