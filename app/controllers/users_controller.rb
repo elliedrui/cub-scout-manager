3903c2cdca1cd5d5714c89afb5c21fb4d57bc7c2
jaesonwatts <illender.the.great@gmail.com>
2020-07-14 18:56:03 -0600

    spelling error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_is] = @user.id
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       render :new
